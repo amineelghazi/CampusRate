ed301c59aa7d7bfae63cc4240772a19a6ac4a944
amineelghazi <amineelghazi100@hotmail.com>
2026-09-22 19:27:42 -0400

    feat(errors): add global ValidationPipe,Problem Details filter & improved project file structure with /common #7

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule} from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+import { ProblemDetailsFilter } from './common/filters/problem-details.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalFilters(new ProblemDetailsFilter());
+
   await app.listen(app.get(ConfigService).getOrThrow<number>('PORT'));
 }
 bootstrap();
-
-
