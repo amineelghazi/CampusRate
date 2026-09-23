7d1148cb730fa1b737ef9738ce2196c20e2b2164
amineelghazi <amineelghazi100@hotmail.com>
2026-09-22 21:54:48 -0400

    Created reviews.controller routes & applied dtos

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,34 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ReviewsService } from './reviews.service';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Res } from '@nestjs/common';
+import type { Response } from 'express';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { ReviewsService } from './reviews.service';
 
-@Controller('reviews')
+@Controller()
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
 
-  @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewsService.create(createReviewDto);
+  @Post('places/:placeId/reviews')
+  @HttpCode(201)
+  async create(
+    @Param('placeId') placeId: string,
+    @Body() dto: CreateReviewDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const review = await this.reviewsService.create(placeId, dto);
+    res.setHeader('Location', `/api/v1/reviews/${review.id}`);
+    return review;
   }
 
-  @Get()
-  findAll() {
-    return this.reviewsService.findAll();
+  @Get('places/:placeId/reviews')
+  findAllForPlace(@Param('placeId') placeId: string) {
+    return this.reviewsService.findAllForPlace(placeId).then((data) => ({ data }));
   }
 
-  @Get(':id')
+  @Get('reviews/:id')
   findOne(@Param('id') id: string) {
-    return this.reviewsService.findOne(+id);
+    return this.reviewsService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto) {
-    return this.reviewsService.update(+id, updateReviewDto);
+  @Patch('reviews/:id')
+  update(@Param('id') id: string, @Body() dto: UpdateReviewDto) {
+    return this.reviewsService.update(id, dto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reviewsService.remove(+id);
+  @Delete('reviews/:id')
+  @HttpCode(204)
+  async remove(@Param('id') id: string) {
+    await this.reviewsService.remove(id);
   }
 }
