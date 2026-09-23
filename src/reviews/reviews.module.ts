6c6f829d851a477168781ddd4b0ca6d998b97cc4
amineelghazi <amineelghazi100@hotmail.com>
2026-09-22 22:21:42 -0400

    feat(reviews) implemented full CRUD for the review resource & tested all routes with Postman #6

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
+  imports: [StorageModule],
   controllers: [ReviewsController],
   providers: [ReviewsService],
 })
