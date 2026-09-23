943759a25855f64dc65fe30f82772a8ceac280fb
amineelghazi <amineelghazi100@hotmail.com>
2026-09-22 21:39:33 -0400

    Added review entity

--- src/reviews/entities/review.entity.ts
+++ src/reviews/entities/review.entity.ts
@@ -1,1 +1,9 @@
-export class Review {}
+export interface Review {
+  id: string;
+  placeId: string;
+  authorName: string;
+  rating: number;
+  comment: string;
+  createdAt: string;
+  updatedAt: string;
+}
