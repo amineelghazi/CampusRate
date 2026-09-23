8d931c1ee959233508fde21eaba8d87e69b732d6
amineelghazi <amineelghazi100@hotmail.com>
2026-09-22 21:41:32 -0400

    Improved create-review.dto

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,17 @@
-export class CreateReviewDto {}
+import { IsInt, IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
+
+export class CreateReviewDto {
+  @IsString()
+  @MinLength(1)
+  authorName: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  rating: number;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(300)
+  comment: string;
+}
